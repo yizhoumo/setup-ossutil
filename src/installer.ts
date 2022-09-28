07200f696cfa41c884b8347e2946373e23d63ea8
Yizhou Mo <yizhoumo@hotmail.com>
2022-09-29 00:17:19 +0800

    call github api to get the latest version info

--- src/installer.ts
+++ src/installer.ts
@@ -1,11 +1,9 @@
 import * as core from '@actions/core'
+import * as github from '@actions/github'
 import * as tc from '@actions/tool-cache'
 import * as fs from 'fs'
-import {HttpClient} from '@actions/http-client'
 
 const ToolName = 'ossutil'
-const UpdateUrl =
-  'https://ossutil-version-update.oss-cn-hangzhou.aliyuncs.com/ossutilversion'
 const DownloadEndpoint = 'https://gosspublic.alicdn.com/ossutil'
 
 /**
@@ -83,11 +81,17 @@ function getDownloadUrl(version: string): string {
  * @returns the latest version
  */
 async function getLatestVersion(): Promise<string> {
-  const http = new HttpClient('setup-ossutil', [], {
-    allowRetries: true,
-    maxRetries: 5
+  const token = core.getInput('github-token', {required: true})
+  const octokit = github.getOctokit(token)
+  const response = await octokit.rest.repos.getLatestRelease({
+    owner: 'aliyun',
+    repo: 'ossutil'
   })
-  const response = await http.get(UpdateUrl)
-  const content = await response.readBody()
-  return content.trim()
+
+  const tag = response.data.tag_name.trim()
+  if (tag[0].toLowerCase() === 'v') {
+    return tag.substring(1)
+  } else {
+    return tag
+  }
 }
